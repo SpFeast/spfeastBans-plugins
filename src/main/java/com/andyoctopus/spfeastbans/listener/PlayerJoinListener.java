6a3059f84c0d2d09b051181237b992f12cbca163
SpFeast <spfeast@icloud.com>
2026-06-11 03:23:06 +0800

    修改了禁言提示的内容

---
+++ src/main/java/com/andyoctopus/spfeastbans/listener/PlayerJoinListener.java
@@ -0,0 +1,25 @@
+package com.andyoctopus.spfeastbans.listener;
+
+import com.andyoctopus.spfeastbans.SpfeastBansPlugin;
+import com.andyoctopus.spfeastbans.mute.MuteService;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public final class PlayerJoinListener implements Listener {
+    private final SpfeastBansPlugin plugin;
+    private final MuteService muteService;
+
+    public PlayerJoinListener(SpfeastBansPlugin plugin, MuteService muteService) {
+        this.plugin = plugin;
+        this.muteService = muteService;
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Bukkit.getScheduler().runTask(plugin, () -> muteService.findActiveMute(event.getPlayer().getUniqueId())
+                .ifPresent(entry -> muteService.sendMuteNotificationIfPending(event.getPlayer(), entry)));
+    }
+}
